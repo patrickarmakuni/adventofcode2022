272acbac7c0bade6273d198df225f81f31938a6c
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-01 12:33:22 +0000

    Split into sublists

--- 1st/1st.hs
+++ 1st/1st.hs
@@ -3,5 +3,18 @@ import System.IO
 main = do
     withFile "input.txt" ReadMode (\handle -> do
         contents <- hGetContents handle
-        putStr contents)
+        let inventoryList = lines contents
+            inventories = splitOn "" inventoryList
+            inventory1 = head inventories
+        putStr $ unlines inventory1)
+
+
+splitOn :: String -> [String] -> [[String]]
+splitOn x ys = splitOnAcc x (reverse ys) [[]] 
+
+splitOnAcc :: String -> [String] -> [[String]] -> [[String]]
+splitOnAcc _ [] acc = acc
+splitOnAcc x (y:ys) (z:zs)
+    | x == y    = splitOnAcc x ys ([]:(z:zs))
+    | otherwise = splitOnAcc x ys ((y:z):zs)
 

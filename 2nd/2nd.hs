b884f9bf1ea78317642b58670779199ba5504833
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-03 14:42:56 +0000

    Calculate scores based on required results

--- 2nd/2nd.hs
+++ 2nd/2nd.hs
@@ -4,12 +4,18 @@ main = do
     contents <- readFile "input.txt"
     let rounds = lines contents
         tuples = map (\x -> (x !! 0, x !! 2)) rounds
-        scores = map (\x -> calculateScore x) tuples
-    print (sum scores)
+        scores1 = map (\x -> calculateScore1 x) tuples
+        scores2 = map (\x -> calculateScore2 x) tuples
+    print $ sum scores1
+    print $ sum scores2
     
 
-calculateScore :: (Char, Char) -> Int
-calculateScore (opponent, response) = (shapeScore response) + (outcomeScore opponent response)
+calculateScore1 :: (Char, Char) -> Int
+calculateScore1 (opponent, response) = (shapeScore response) + (outcomeScore opponent response)
+
+calculateScore2 :: (Char, Char) -> Int
+calculateScore2 (opponent, result) = (shapeScore response) + (outcomeScore opponent response)
+    where response = requiredMove opponent result
     
 shapeScore :: Char -> Int
 shapeScore 'X' = 1
@@ -26,4 +32,15 @@ outcomeScore 'C' 'Z' = 3
 outcomeScore 'A' 'Y' = 6
 outcomeScore 'B' 'Z' = 6
 outcomeScore 'C' 'X' = 6
+
+requiredMove :: Char -> Char -> Char
+requiredMove 'A' 'X' = 'Z'
+requiredMove 'A' 'Y' = 'X'
+requiredMove 'A' 'Z' = 'Y'
+requiredMove 'B' 'X' = 'X'
+requiredMove 'B' 'Y' = 'Y'
+requiredMove 'B' 'Z' = 'Z'
+requiredMove 'C' 'X' = 'Y'
+requiredMove 'C' 'Y' = 'Z'
+requiredMove 'C' 'Z' = 'X'
 

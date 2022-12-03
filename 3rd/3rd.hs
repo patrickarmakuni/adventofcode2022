1ea096317ca1059160b7d35ca2c54a313e90c641
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-03 16:22:13 +0000

    Calculate sum of threes scores

--- 3rd/3rd.hs
+++ 3rd/3rd.hs
@@ -5,12 +5,16 @@ import Data.Maybe
 main = do
     contents <- readFile "input.txt"
     let rucksacks = lines contents
-        scores = map (\x -> calculateScore x) rucksacks
-    print $ sum scores
+    print $ part1 rucksacks
+    print $ part2 rucksacks
     
 
-calculateScore :: String -> Int
-calculateScore s = getScore $ identifyMatching $ splitString s
+part1 :: [String] -> Int
+part1 rucksacks = sum $ map (getScore . identifyMatching . splitString) rucksacks
+
+part2 :: [String] -> Int
+part2 rucksacks = sum $ map (getScore . identifyMatching3) $ getThrees rucksacks
+
 
 splitString :: String -> (String, String)
 splitString s = splitAt halfway s
@@ -22,4 +26,11 @@ identifyMatching (x, y) = foldl1 (\acc c -> if elem c y then c else acc) x
 getScore :: Char -> Int
 getScore c = (fromMaybe 0 (elemIndex c priorityList)) + 1
     where priorityList = ['a'..'z'] ++ ['A'..'Z']
+
+getThrees :: [String] -> [(String, String, String)]
+getThrees [] = []
+getThrees (a:b:c:xs) = [(a, b, c)] ++ (getThrees xs)
+
+identifyMatching3 :: (String, String, String) -> Char
+identifyMatching3 (x, y, z) = foldl1 (\acc c -> if (elem c y) && (elem c z) then c else acc) x
 

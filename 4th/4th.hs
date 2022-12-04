af092e87c676a31784a4be1afc333f58f024d315
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-04 14:51:41 +0000

    Count fully contained pairs

--- 4th/4th.hs
+++ 4th/4th.hs
@@ -8,8 +8,8 @@ main = do
     print $ part1 pairs
     
 
-part1 :: [String] -> [Pair]
-part1 pairs = map (parsePair) pairs
+part1 :: [String] -> Int
+part1 pairs = length $ filter (isFullyContained) $ map (parsePair) pairs
 
 type Range = (Int, Int)
 type Pair = (Range, Range)
@@ -25,4 +25,10 @@ parseRange range = (read fst, read snd)
 splitOn :: Eq a => a -> [a] -> ([a], [a])
 splitOn elem list = (take index list, drop (index + 1) list)
     where index = fromMaybe 0 (elemIndex elem list)
+
+isFullyContained :: Pair -> Bool
+isFullyContained (range1, range2) = (range1 `fullyContains` range2) || (range2 `fullyContains` range1)
+
+fullyContains :: Range -> Range -> Bool
+(lower1, upper1) `fullyContains` (lower2, upper2) = (lower1 <= lower2) && (upper1 >= upper2)
 

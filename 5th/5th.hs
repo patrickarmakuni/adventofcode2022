2fdecd9375ab8161150a48634dd5e4f84df2e976
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-05 20:34:29 +0000

    Process moves and get top crates

--- 5th/5th.hs
+++ 5th/5th.hs
@@ -6,13 +6,28 @@ import Data.Char
 main = do
     contents <- readFile "input.txt"
     let (stacks, moves) = parseInput (lines contents)
-    print stacks
-    print moves
+    print $ part1 stacks moves
 
 
 type Stack = [Char]
 type Row = [Char]
 type Move = (Int, Int, Int)
+
+
+part1 :: [Stack] -> [Move] -> String
+part1 stacks moves = map (head) (processMoves stacks moves)
+    where processMoves stacks moves = foldl (\acc m -> processMove acc m) stacks moves
+
+
+processMove :: [Stack] -> Move -> [Stack]
+processMove stacks (0, _, _) = stacks
+processMove stacks (number, origin, destination) = processMove (move1 stacks origin destination) (number - 1, origin, destination)
+
+move1 :: [Stack] -> Int -> Int -> [Stack]
+move1 stacks origin destination = (addToDestination . removeFromOrigin) stacks
+    where crate = head (stacks !! origin)
+          removeFromOrigin s = (take origin s) ++ [(tail (s !! origin))] ++ (drop (origin + 1) s)
+          addToDestination s = (take destination s) ++ [(crate : (s !! destination))] ++ (drop (destination + 1) s)
 
 parseInput :: [String] -> ([Stack], [Move])
 parseInput input = (parseStacks stacks, parseMoves moves)
@@ -22,7 +37,7 @@ parseMoves :: [String] -> [(Int, Int, Int)]
 parseMoves moves = map (parseMove) moves
 
 parseMove :: String -> (Int, Int, Int)
-parseMove move = (getInt ws 1, getInt ws 3, getInt ws 5)
+parseMove move = (getInt ws 1, (getInt ws 3) - 1, (getInt ws 5) - 1)
     where ws = words move
           getInt ws idx = read (ws !! idx)
 

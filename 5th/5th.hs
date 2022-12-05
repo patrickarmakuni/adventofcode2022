4d1ee0590eac73af7994031a6ade1dd7ef54f9ab
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-05 14:47:46 +0000

    Trim whitespace

--- 5th/5th.hs
+++ 5th/5th.hs
@@ -1,6 +1,7 @@
 import System.IO
 import Data.List
 import Data.Maybe
+import Data.Char
 
 main = do
     contents <- readFile "input.txt"
@@ -9,6 +10,7 @@ main = do
 
 
 type Stack = [Char]
+type Row = [Char]
 type Move = String
 
 parseInput :: [String] -> ([Stack], [Move])
@@ -16,18 +18,21 @@ parseInput input = (parseStacks stacks, moves)
     where (stacks, moves) = splitOn "" input
 
 parseStacks :: [String] -> [Stack]
-parseStacks stacks = transpose rows
+parseStacks stacks = map (trim) (transpose rows)
     where rows = parseRows actualStacks
           actualStacks = init stacks
+          trim = dropWhileEnd isSpace . dropWhile isSpace
 
-parseRows :: [String] -> [[Char]]
+parseRows :: [String] -> [Row]
 parseRows [] = []
 parseRows (top:rows) = (parseRow top) : parseRows rows
 
-parseRow :: String -> [Char]
+parseRow :: String -> Row
 parseRow str = map (\idx -> str !! idx) indices
     where indices = [1,5..(length str)]
 
 splitOn :: Eq a => a -> [a] -> ([a], [a])
 splitOn elem list = (take index list, drop (index + 1) list)
     where index = fromMaybe 0 (elemIndex elem list)
+
+

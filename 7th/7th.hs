4291f1ddbf271680d808ccabe0f2afdbdcdc6611
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-07 18:07:45 +0000

    Calculate sum of all files

--- 7th/7th.hs
+++ 7th/7th.hs
@@ -1,34 +1,47 @@
 import System.IO
 import Data.List
+import Data.Char
 
 main = do
-    contents <- readFile "testInput.txt"
+    contents <- readFile "input.txt"
     let output = lines contents
     print $ part1 output
 
 
 part1 :: [String] -> Int
-part1 output = sum $ filter (<= 100000) $ map (calculateSize output) $ directories output
+part1 output = getSize output
 
-calculateSize :: [String] -> String -> Int
-calculateSize output directory = foldl (\acc line -> if isFile line then acc + (getFileSize line) else acc + (calculateSize output (drop 4 line))) 0 contents
-    where contents = getDirContents output directory
-          isFile = not . isDirectory
-          isDirectory = isPrefixOf "dir"
+getSize :: [String] -> Int
+getSize [] = 0
+getSize input = sumFilesInDir + sumSubdirs
+    where sumFilesInDir = sum $ map (getFileSize) $ filter (isFile) $ getCurrentDirContents input
+          sumSubdirs = sum $ subdirSizes
+          subdirSizes = map (getSize) $ getBlocks nextSection
+          nextSection = dropWhile (not . isCommand) $ dropWhile (isCommand) input
+          isCommand = isPrefixOf "$ "
+          isFile = (isDigit . head)
 
-getDirContents :: [String] -> String -> [String]
-getDirContents output dir = takeWhile (not . isCommand) $ dropWhile (isCommand) $ dropWhile (/= "$ cd " ++ dir) output
+getCurrentDirContents :: [String] -> [String]
+getCurrentDirContents output = takeWhile (not . isCommand) $ dropWhile (isCommand) output
     where isCommand = isPrefixOf "$ "
 
+getBlocks :: [String] -> [[String]]
+getBlocks input = splitAt' idxs input
+    where idxs = getSplitIndices input 0 0 []
+
+splitAt' :: (Eq a) => [Int] -> [a] -> [[a]]
+splitAt' [] input = [input]
+splitAt' (idx:idxs) input = [(take idx input)] ++ (splitAt' (map (subtract idx) idxs) (drop (idx + 1) input))
+
+getSplitIndices :: [String] -> Int -> Int -> [Int] -> [Int]
+getSplitIndices [] _ _ acc = acc
+getSplitIndices (line:rest) level idx acc
+    | isCdUp line = if level == 1 then getSplitIndices rest 0 (idx + 1) (idx : acc) else getSplitIndices rest (level - 1) (idx + 1) acc
+    | isCdDown line = getSplitIndices rest (level + 1) (idx + 1) acc
+    | otherwise = getSplitIndices rest level (idx + 1) acc
+    where isCdUp l = l == "$ cd .."
+          isCdDown l = "$ cd" `isPrefixOf` l
+
 getFileSize :: String -> Int
-getFileSize line = read $ head $ words line
-
-directories :: [String] -> [String]
-directories output = "/" : (unique $ map (drop 4) $ filter ("dir" `isPrefixOf`) output)
-
-unique :: (Eq a) => [a] -> [a]
-unique [] = []
-unique (x:xs)
-    | any (== x) xs = unique xs
-    | otherwise = x : unique xs
+getFileSize = read . head . words
 

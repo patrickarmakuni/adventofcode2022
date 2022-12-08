e36156fc461cd05b9361bfb5c7f69a35ae61b59f
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-08 12:26:18 +0000

    Calculate scenic score

--- 8th/8th.hs
+++ 8th/8th.hs
@@ -5,18 +5,16 @@ import Data.List
 main = do
     contents <- readFile "input.txt"
     let grid = readDigits $ lines contents
-    print $ gridCount $ sumGrids [vfl grid, vfr grid, vft grid, vfb grid]
+    print $ part1 grid
+    print $ part2 grid
 
 
 type Grid = [Row]
 type Row = [Int]
 
-gridCount :: Grid -> Int
-gridCount = sum . map count
-    where count = length . filter (> 0)
 
-sumGrids :: [Grid] -> Grid
-sumGrids = foldl1 (zipWith $ zipWith (+))
+part1 :: Grid -> Int
+part1 grid =  gridCount (> 0) $ gridSum [vfl grid, vfr grid, vft grid, vfb grid]
 
 vfl :: Grid -> Grid
 vfl = map visibleFromLeft
@@ -41,6 +39,51 @@ visibleFromLeftAcc _ acc [] = acc
 visibleFromLeftAcc highest acc (tree:row)
     | tree > highest = visibleFromLeftAcc tree (acc ++ [1]) row
     | otherwise      = visibleFromLeftAcc highest (acc ++ [0]) row
+
+
+part2 :: Grid -> Int
+part2 grid = maximum $ map maximum $ gridProduct [vtl grid, vtr grid, vtt grid, vtb grid]
+
+vtr :: Grid -> Grid
+vtr = map vtrRow
+
+vtl :: Grid -> Grid
+vtl = map vtlRow
+
+vtt :: Grid -> Grid
+vtt = transpose . vtl . transpose
+
+vtb :: Grid -> Grid
+vtb = transpose . vtr . transpose
+
+vtlRow :: Row -> Row
+vtlRow = reverse . vtrRow . reverse
+
+vtrRow :: Row -> Row
+vtrRow row = map visibleTreesRight subrows
+    where subrows = init $ scanr (:) [] row
+
+visibleTreesRight :: Row -> Int
+visibleTreesRight [] = 0
+visibleTreesRight (tree:[]) = 0
+visibleTreesRight (tree:row) = length (takeUntil (>= tree) row)
+
+
+gridCount :: (Int -> Bool) -> Grid -> Int
+gridCount p = sum . map count
+    where count = length . filter p
+
+gridSum :: [Grid] -> Grid
+gridSum = foldl1 (zipWith $ zipWith (+))
+
+gridProduct :: [Grid] -> Grid
+gridProduct = foldl1 (zipWith $ zipWith (*))
+
+takeUntil :: (a -> Bool) -> [a] -> [a]
+takeUntil _ [] = []
+takeUntil p (x:xs)
+    | p x = [x]
+    | otherwise = x : takeUntil p xs
 
 readDigits :: [String] -> Grid
 readDigits = map $ map digitToInt

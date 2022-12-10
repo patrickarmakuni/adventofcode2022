91ba962ef18ce09cde772aa2433144acaceb1248
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-10 10:38:54 +0000

    Print CRT output

--- 9th/9th.hs
+++ 9th/9th.hs
@@ -4,6 +4,7 @@ main = do
     contents <- readFile "input.txt"
     let instructions = lines contents
     print $ part1 instructions
+    putStr $ part2 instructions
 
 
 part1 :: [String] -> Int
@@ -25,3 +26,23 @@ updateState (x:xs) "noop" = x:x:xs
 updateState (x:xs) instruction = addx:x:x:xs
     where addx = x + (read . last . words) instruction
 
+
+part2 :: [String] -> String
+part2 = breakIntoLines . drawPixels . getStates
+
+breakIntoLines :: String -> String
+breakIntoLines [] = []
+breakIntoLines s = a ++ "\n" ++ (breakIntoLines b)
+    where (a, b) = splitAt 40 s
+
+drawPixels :: [Int] -> String
+drawPixels = dpIter 0
+
+dpIter :: Int -> [Int] -> String
+dpIter _ [] = []
+dpIter i (x:xs) = drawChar : dpIter (i + 1) xs
+    where drawChar = if spriteAligns i x then '#' else '.'
+
+spriteAligns :: Int -> Int -> Bool
+spriteAligns cycle x = abs (pixel - x) <= 1
+    where pixel = cycle `mod` 40

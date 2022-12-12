437aa81fc688a15056c3a78cad6ebf00f5659d53
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-12 16:18:49 +0000

    Compute tail moves

--- 9th/9th.hs
+++ 9th/9th.hs
@@ -3,7 +3,8 @@ import System.IO
 main = do
     contents <- readFile "testInput.txt"
     let input = lines contents
-    print $ follow $ convert input
+    print $ headPositions $ convert input
+    print $ tailPositions $ headPositions $ convert input
 
 
 type Direction = Char
@@ -17,12 +18,24 @@ expand s = replicate n c
     where c = head s
           n = read $ last $ words s
 
-follow :: [Direction] -> [Position]
-follow = scanl move (0, 0)
+headPositions :: [Direction] -> [Position]
+headPositions = scanl move (0, 0)
 
 move :: Position -> Direction -> Position
 move (x, y) 'L' = (x - 1, y)
 move (x, y) 'R' = (x + 1, y)
 move (x, y) 'U' = (x, y + 1)
 move (x, y) 'D' = (x, y - 1)
+
+tailPositions :: [Position] -> [Position]
+tailPositions = tail . scanl follow (0, 0)
+
+follow :: Position -> Position -> Position
+follow tail@(xt, yt) head@(xh, yh)
+    | isTouching tail head = tail
+    | abs (xt - xh) < 2   = (xh, (yt + yh) `div` 2)
+    | abs (yt - yh) < 2   = ((xt + xh) `div` 2, yh)
+
+isTouching :: Position -> Position -> Bool
+isTouching (x1, y1) (x2, y2) = abs (x1 - x2) < 2 && abs (y1 - y2) < 2
 

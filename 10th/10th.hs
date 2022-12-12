14fd7ff91f763a7d8395b640ddb1f0038534b5cc
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-12 11:31:35 +0000

    Refactor

--- 10th/10th.hs
+++ 10th/10th.hs
@@ -15,16 +15,19 @@ part1 :: [Monkey] -> Int -> Int
 part1 monkeys n = monkeyBusiness $ runRounds n monkeys
 
 
-type Monkey = ([Int], Int -> Int, Int -> Bool, (Int, Int), Int)
+type Monkey = ([Item], Operation, TestValue, (Int, Int), Int)
+type Item = Int
+type Operation = Int -> Int
+type TestValue = Int
 
-getItems :: Monkey -> [Int]
+getItems :: Monkey -> [Item]
 getItems (items, _, _, _, _) = items
 
-getOperation :: Monkey -> (Int -> Int)
+getOperation :: Monkey -> Operation
 getOperation (_, operation, _, _, _) = operation
 
-getTest :: Monkey -> (Int -> Bool)
-getTest (_, _, test, _, _) = test
+getTestValue :: Monkey -> TestValue
+getTestValue (_, _, testValue, _, _) = testValue
 
 getReceivers :: Monkey -> (Int, Int)
 getReceivers (_, _, _, receivers, _) = receivers
@@ -38,16 +41,16 @@ parseMonkeys :: [String] -> [Monkey]
 parseMonkeys = map parseMonkey . splitOn ""
 
 parseMonkey :: [String] -> Monkey
-parseMonkey input = (items, operation, test, receivers, 0)
+parseMonkey input = (items, operation, testValue, receivers, 0)
     where items = map parseInt $ drop 2 $ words $ input !! 1
           operation = parseOperation $ input !! 2
-          test = parseTest $ input !! 3
+          testValue = parseTestValue $ input !! 3
           receivers = (digitToInt $ last $ input !! 4, digitToInt $ last $ input !! 5)
 
 parseInt :: String -> Int
 parseInt = read . takeWhile isDigit
 
-parseOperation :: String -> (Int -> Int)
+parseOperation :: String -> Operation
 parseOperation s = (\x -> operator (parseOperand x (ws !! 5)) x)
     where operator = parseOperator (ws !! 4)
           ws = words s
@@ -60,9 +63,8 @@ parseOperand :: Int -> String -> Int
 parseOperand x "old" = x
 parseOperand x int = read int
 
-parseTest :: String -> (Int -> Bool)
-parseTest s = (\x -> x `mod` y == 0)
-    where y = read $ last $ words s
+parseTestValue :: String -> TestValue
+parseTestValue = read . last . words
 
 
 monkeyBusiness :: [Monkey] -> Int
@@ -72,7 +74,7 @@ runRounds :: Int -> [Monkey] -> [Monkey]
 runRounds n monkeys = (iterate runRound monkeys) !! n
 
 runRound :: [Monkey] -> [Monkey]
-runRound monkeys = iterateTurns 0 monkeys
+runRound = iterateTurns 0
 
 iterateTurns :: Int -> [Monkey] -> [Monkey]
 iterateTurns _ [] = []
@@ -82,15 +84,15 @@ iterateTurns i monkeys
 
 takeTurn :: Monkey -> Int -> [Monkey] -> [Monkey]
 takeTurn monkey@([], _, _, _, _) i monkeys = updateList i monkey monkeys
-takeTurn (input:items, operation, test, receivers, itemsInspected) i monkeys = takeTurn updateCurrent i (updateList receiver (updateMonkey (monkeys !! receiver) output) monkeys)
-    where (output, receiver) = calculateOutput input operation test receivers
-          updateCurrent = (items, operation, test, receivers, itemsInspected + 1)
+takeTurn (input:items, operation, testValue, receivers, itemsInspected) i monkeys = takeTurn updateCurrent i (updateList receiver (updateMonkey (monkeys !! receiver) output) monkeys)
+    where (output, receiver) = calculateOutput input operation testValue receivers
+          updateCurrent = (items, operation, testValue, receivers, itemsInspected + 1)
 
-calculateOutput :: Int -> (Int -> Int) -> (Int -> Bool) -> (Int, Int) -> (Int, Int)
-calculateOutput input operation test receivers = (output, if test output then fst receivers else snd receivers)
+calculateOutput :: Item -> Operation -> TestValue -> (Int, Int) -> (Int, Int)
+calculateOutput input operation testValue receivers = (output, if (output `mod` testValue) == 0 then fst receivers else snd receivers)
     where output = (operation input) `div` 3
 
-updateMonkey :: Monkey -> Int -> Monkey
+updateMonkey :: Monkey -> Item -> Monkey
 updateMonkey (items, a, b, c, d) item = (items ++ [item], a, b, c, d)
 
 updateList :: Int -> a -> [a] -> [a]

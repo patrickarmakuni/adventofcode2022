5e8ae3b9ab79ce26a89f01cc9afa467e1aac8daf
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-12 15:55:00 +0000

    Compute head moves

--- 9th/9th.hs
+++ 9th/9th.hs
@@ -3,10 +3,11 @@ import System.IO
 main = do
     contents <- readFile "testInput.txt"
     let input = lines contents
-    print $ convert input
+    print $ follow $ convert input
 
 
 type Direction = Char
+type Position = (Int, Int)
 
 convert :: [String] -> [Direction]
 convert = concat . map expand
@@ -15,4 +16,13 @@ expand :: String -> [Direction]
 expand s = replicate n c
     where c = head s
           n = read $ last $ words s
+
+follow :: [Direction] -> [Position]
+follow = scanl move (0, 0)
+
+move :: Position -> Direction -> Position
+move (x, y) 'L' = (x - 1, y)
+move (x, y) 'R' = (x + 1, y)
+move (x, y) 'U' = (x, y + 1)
+move (x, y) 'D' = (x, y - 1)
 

9d35058713d6ba5fea2a3a436694d0749f7318ef
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-14 12:51:48 +0000

    Calculate trajectory of grain

--- 14th/14th.hs
+++ 14th/14th.hs
@@ -7,6 +7,22 @@ main = do
 
 type Position = (Int, Int)
 
+fallsEndlessly :: [Position] -> Int -> Position -> Bool
+fallsEndlessly blocked floor start = snd (trajectory !! (floor + 1)) > floor
+    where trajectory = iterate (fallStep blocked) start
+
+dropGrain :: Position -> Int -> [Position] -> [Position]
+dropGrain start floor blocked = (fall blocked start floor) : blocked
+
+fall :: [Position] -> Position -> Int -> Position
+fall blocked start floor = trajectory !! floor
+    where trajectory = iterate (fallStep blocked) start
+
+restingPosition :: [Position] -> Position
+restingPosition (x:y:xs)
+    | x == y    = x
+    | otherwise = restingPosition xs
+
 fallStep :: [Position] -> Position -> Position
 fallStep blocked current@(x, y) = getCandidate blocked current candidates
     where candidates = [(x, y + 1), (x - 1, y + 1), (x + 1, y + 1)]

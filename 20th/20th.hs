040abfaf11ab5a58e1637cbc7a542037aa6baf96
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-20 16:55:42 +0000

    Implement chained mixing

--- 20th/20th.hs
+++ 20th/20th.hs
@@ -9,22 +9,43 @@ main = do
     let inputFile = args !! 0
     input <- fmap lines $ readFile inputFile
     let numbers = map read input :: [Int]
-    print $ sum $ coordinates $ mix numbers
+    print $ part1 numbers
+    print $ part2 numbers
 
 
 type Index = Int
 
 
-mix :: [Int] -> [Int]
-mix numbers = mixWithIndices [0..(length numbers - 1)] numbers
+part1 :: [Int] -> Int
+part1 numbers = sum $ coordinates $ valuesAt (mix numbers indices) numbers
+    where indices = [0..(length numbers - 1)]
 
-mixWithIndices :: [Index] -> [Int] -> [Int]
-mixWithIndices [] numbers = numbers
-mixWithIndices (idx:indices) numbers = mixWithIndices newIndices newNumbers
-    where newNumbers = relocate idx destination numbers
-          newIndices = map (updateIndex idx destination) indices
-          destination = (idx + value) `mod` (length numbers - 1)
-          value = numbers !! idx
+part2 :: [Int] -> Int
+part2 numbers = sum $ coordinates $ valuesAt mix10 decrypted
+    where decrypted = map (* 811589153) numbers
+          indices = [0..(length numbers - 1)]
+          mix10 = (iterate (mix decrypted) indices) !! 10
+
+
+getOrder :: Index -> [Index] -> [Index]
+getOrder x xs
+    | x >= length xs = []
+    | otherwise = idx : getOrder (x + 1) xs
+    where idx = fromJust $ elemIndex x xs
+
+valuesAt :: [Index] -> [a] -> [a]
+valuesAt indices xs = map (\idx -> xs !! idx) indices
+
+mix :: [Int] -> [Index] -> [Index]
+mix numbers indices = mixAcc order indices numbers
+    where order = getOrder 0 indices
+
+mixAcc :: [Index] -> [Index] -> [Int] -> [Int]
+mixAcc [] state _ = state
+mixAcc (idx:indices) state (value:numbers) = mixAcc remaining newState numbers
+    where newState = relocate idx destination state
+          remaining = map (updateIndex idx destination) indices
+          destination = (idx + value) `mod` (length state - 1)
 
 updateIndex :: Index -> Index -> Index -> Index
 updateIndex from to idx

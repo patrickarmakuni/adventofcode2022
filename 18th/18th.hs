0389e7f18343ee79579795ac710197bfeed99a4d
Patrick McGovern <patrick.mcgovern@armakuni.com>
2022-12-24 16:52:24 +0000

    Calculate surface area excluding trapped air

--- 18th/18th.hs
+++ 18th/18th.hs
@@ -7,14 +7,46 @@ main = do
     let inputFile = args !! 0
     input <- fmap lines $ readFile inputFile
     let droplet = map parseCube input
-    print $ surfaceArea droplet
+    print $ part1 droplet
+    print $ part2 droplet
 
 
 type Cube = (Int, Int, Int)
+type Bounds1 = (Int, Int)
+type Bounds3 = (Bounds1, Bounds1, Bounds1)
 
 
-surfaceArea :: [Cube] -> Int
-surfaceArea droplet = sum $ map (\c -> exposed c droplet) droplet
+part2 :: [Cube] -> Int
+part2 droplet = sum $ map (\c -> exposed' c droplet) droplet
+
+exposed' :: Cube -> [Cube] -> Int
+exposed' cube droplet = length $ filter (isFreeAir b droplet) $ neighbours cube
+    where b = bounds droplet
+
+isFreeAir :: Bounds3 -> [Cube] -> Cube -> Bool
+isFreeAir bounds cubes cube
+    | isOutOfBounds cube bounds = True
+    | cubes `contains` cube = False
+    | otherwise = any (isFreeAir bounds cubes) $ neighbours cube
+
+isOutOfBounds :: Cube -> Bounds3 -> Bool
+isOutOfBounds (x, y, z) ((x0, x1), (y0, y1), (z0, z1)) =
+                x < x0 || x > x1 ||
+                y < y0 || y > y1 ||
+                z < z0 || z > z1
+
+neighbours :: Cube -> [Cube]
+neighbours (x, y, z) = [(x + 1, y, z), (x - 1, y, z),
+                        (x, y + 1, z), (x, y - 1, z),
+                        (x, y, z + 1), (x, y, z - 1)]
+
+bounds :: [Cube] -> Bounds3
+bounds droplet = (\(x, y, z) -> (b x, b y, b z)) $ unzip3 droplet
+    where b xs = (minimum xs, maximum xs)
+
+
+part1 :: [Cube] -> Int
+part1 droplet = sum $ map (\c -> exposed c droplet) droplet
 
 exposed :: Cube -> [Cube] -> Int
 exposed cube others = 6 - countAdjacent cube others
@@ -38,4 +70,7 @@ wordsWhen p s = case dropWhile p s of
                      "" -> []
                      s' -> w : wordsWhen p s''
                         where (w, s'') = break p s'
+
+contains :: (Eq a) => [a] -> a -> Bool
+xs `contains` x = any (== x) xs
 

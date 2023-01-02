244ee2534feccba8e563bb276d1868e36dad7406
Patrick McGovern <patrick.mcgovern@armakuni.com>
2023-01-02 11:35:34 +0000

    Find shortest path from any start point

--- 12th/12th.hs
+++ 12th/12th.hs
@@ -8,7 +8,7 @@ main = do
     args <- getArgs
     let inputFile = args !! 0
     input <- fmap lines $ readFile inputFile
-    print $ part1 input
+    print $ part2 input
 
 
 type Position = (Int, Int)
@@ -21,7 +21,23 @@ part1 hmap = iterateSearch 0 [start] [start] end hmap
           end = getPosition 'E' hmap
 
 
+part2 :: Heightmap -> Int
+part2 hmap = iterateStartPositions (0, 0) hmap 1000000
+
+
+iterateStartPositions :: Position -> Heightmap -> Int -> Int
+iterateStartPositions pos@(x, y) hmap acc
+    | x + 1 < width = iterateStartPositions (x + 1, y) hmap getShortest
+    | y + 1 < height = iterateStartPositions (0, y + 1) hmap getShortest
+    | otherwise = acc
+    where getShortest = if heightAt pos hmap > 0 then acc else min acc $ shortestPath
+          shortestPath = iterateSearch 0 [pos] [pos] end hmap
+          end = getPosition 'E' hmap
+          (width, height) = dimensions hmap
+
+
 iterateSearch :: Int -> [Position] -> [Position] -> Position -> Heightmap -> Int
+iterateSearch _ [] _ _ _ = 1000000
 iterateSearch t current searched end hmap
     | any (==end) current = t
     | otherwise = iterateSearch (t + 1) next (next ++ searched) end hmap
@@ -42,13 +58,20 @@ tooHigh :: Heightmap -> Position -> Position -> Bool
 tooHigh hmap current neighbour = height neighbour > height current + 1
     where height (x, y) = heightValue $ (hmap !! y) !! x
 
+heightAt :: Position -> Heightmap -> Int
+heightAt (x, y) hmap = heightValue $ (hmap !! y) !! x
+
 heightValue :: Char -> Int
 heightValue 'S' = heightValue 'a'
 heightValue 'E' = heightValue 'z'
 heightValue c = fromJust $ elemIndex c ['a'..'z']
 
 isOnMap :: Heightmap -> Position -> Bool
-isOnMap hmap (x, y) = x >= 0 && y >= 0 && x < (width hmap) && y < (height hmap)
-    where width hmap = length (head hmap)
-          height hmap = length hmap
+isOnMap hmap (x, y) = x >= 0 && y >= 0 && x < width && y < height
+    where (width, height) = dimensions hmap
+
+dimensions :: Heightmap -> (Int, Int)
+dimensions hmap = (width, height)
+    where width = length (head hmap)
+          height = length hmap
 

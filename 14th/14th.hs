0dc6469adbb712fd0d6c007f527a963a02f71fba
Patrick McGovern <patrick.mcgovern@armakuni.com>
2023-01-06 07:07:10 +0000

    Rearrange code

--- 14th/14th.hs
+++ 14th/14th.hs
@@ -17,7 +17,7 @@ type Row = [Int]
 
 
 part1 :: [String] -> Int
-part1 input = findSteadyState 0 $ iterate (dropGrain (500, 0) floor) rows
+part1 input = findSteadyState 0 $ iterate (dropGrain1 (500, 0) floor) rows
     where blocked = getStructures input
           rows = convertToRows (floor + 1) blocked
           floor = maximum $ map snd blocked
@@ -27,17 +27,53 @@ findSteadyState idx (t1:t2:ts)
     | t1 == t2 = idx
     | otherwise = findSteadyState (idx + 1) (t2:ts)
 
+dropGrain1 :: Position -> Int -> [Row] -> [Row]
+dropGrain1 start floor blocked = addGrain1 (fall1 blocked floor start) blocked
+
+addGrain1 :: Maybe Position -> [Row] -> [Row]
+addGrain1 Nothing rows = rows
+addGrain1 (Just (x, y)) rows = (take y rows) ++ [(x : (rows !! y))] ++ (drop (y+1) rows)
+
+fall1 :: [Row] -> Int -> Position -> Maybe Position
+fall1 blocked floor current@(x, y)
+    | y >= floor = Nothing
+    | next == current = Just current
+    | otherwise = fall1 blocked floor next
+    where next = fallStep blocked current
+
 
 part2 :: [String] -> Int
-part2 input = length $ takeWhile (\rows -> null (rows !! 0)) $ iterate (dropGrain' (500, 0) floor) rows
+part2 input = length $ takeWhile (\rows -> null (rows !! 0)) $ iterate (dropGrain2 (500, 0) floor) rows
     where blocked = getStructures input
           rows = convertToRows floor blocked
           floor = (maximum $ map snd blocked) + 2
 
+dropGrain2 :: Position -> Int -> [Row] -> [Row]
+dropGrain2 start floor blocked = addGrain2 (fall2 blocked floor start) blocked
+
+addGrain2 :: Position -> [Row] -> [Row]
+addGrain2 (x, y) rows = (take y rows) ++ [(x : (rows !! y))] ++ (drop (y+1) rows)
+
+fall2 :: [Row] -> Int -> Position -> Position
+fall2 blocked floor current@(x, y)
+    | y + 1 == floor = current
+    | next == current = current
+    | otherwise = fall2 blocked floor next
+    where next = fallStep blocked current
+
+
+fallStep :: [Row] -> Position -> Position
+fallStep blocked current@(x, y)
+    | isFree x          = (x, y + 1)
+    | isFree (x - 1)    = (x - 1, y + 1)
+    | isFree (x + 1)    = (x + 1, y + 1)
+    | otherwise         = current
+    where isFree x = all (/= x) (blocked !! (y + 1))
+
+
 convertToRows :: Int -> [Position] -> [Row]
 convertToRows floor positions = reverse $ foldl (\acc y -> (getRow y) : acc) [] [0..(floor-1)]
     where getRow row = map fst $ filter (\(_, y) -> y == row) positions
-
 
 getStructures :: [String] -> [Position]
 getStructures input = concat $ map (buildStructure . points) input
@@ -58,40 +94,6 @@ drawLine (x1, y1) (x2, y2)
 parsePosition :: String -> Position
 parsePosition s = (read x, read $ tail y)
     where (x, y) = splitAt 3 s
-
-
-dropGrain :: Position -> Int -> [Row] -> [Row]
-dropGrain start floor blocked = addGrain (fall blocked floor start) blocked
-
-dropGrain' :: Position -> Int -> [Row] -> [Row]
-dropGrain' start floor blocked = addGrain (Just $ fall' blocked floor start) blocked
-
-addGrain :: Maybe Position -> [Row] -> [Row]
-addGrain Nothing rows = rows
-addGrain (Just (x, y)) rows = (take y rows) ++ [(x : (rows !! y))] ++ (drop (y+1) rows)
-
-
-fall :: [Row] -> Int -> Position -> Maybe Position
-fall blocked floor current@(x, y)
-    | y >= floor = Nothing
-    | next == current = Just current
-    | otherwise = fall blocked floor next
-    where next = fallStep blocked current
-
-fall' :: [Row] -> Int -> Position -> Position
-fall' blocked floor current@(x, y)
-    | y + 1 == floor = current
-    | next == current = current
-    | otherwise = fall' blocked floor next
-    where next = fallStep blocked current
-
-fallStep :: [Row] -> Position -> Position
-fallStep blocked current@(x, y)
-    | isFree x          = (x, y + 1)
-    | isFree (x - 1)    = (x - 1, y + 1)
-    | isFree (x + 1)    = (x + 1, y + 1)
-    | otherwise         = current
-    where isFree x = all (/= x) (blocked !! (y + 1))
 
 
 splitOn :: String -> [String] -> [[String]]

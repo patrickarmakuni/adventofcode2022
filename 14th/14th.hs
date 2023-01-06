e232d9d9f19dcbf039faadd77dadd958d4d6d0b8
Patrick McGovern <patrick.mcgovern@armakuni.com>
2023-01-06 07:39:58 +0000

    Refactor

--- 14th/14th.hs
+++ 14th/14th.hs
@@ -17,50 +17,31 @@ type Row = [Int]
 
 
 part1 :: [String] -> Int
-part1 input = findSteadyState 0 $ iterate (dropGrain1 (500, 0) floor) rows
-    where blocked = getStructures input
-          rows = convertToRows (floor + 1) blocked
-          floor = maximum $ map snd blocked
-
-findSteadyState :: Int -> [[Row]] -> Int
-findSteadyState idx (t1:t2:ts)
-    | t1 == t2 = idx
-    | otherwise = findSteadyState (idx + 1) (t2:ts)
-
-dropGrain1 :: Position -> Int -> [Row] -> [Row]
-dropGrain1 start floor blocked = addGrain1 (fall1 blocked floor start) blocked
-
-addGrain1 :: Maybe Position -> [Row] -> [Row]
-addGrain1 Nothing rows = rows
-addGrain1 (Just (x, y)) rows = (take y rows) ++ [(x : (rows !! y))] ++ (drop (y+1) rows)
-
-fall1 :: [Row] -> Int -> Position -> Maybe Position
-fall1 blocked floor current@(x, y)
-    | y >= floor = Nothing
-    | next == current = Just current
-    | otherwise = fall1 blocked floor next
-    where next = fallStep blocked current
-
+part1 input = (subtract 1) $ countWhile floorIsNotReached structures
+    where structures = addFloor $ convertToRows $ getStructures input
+          floorIsNotReached rows = null (rows !! ((length rows) - 2))
 
 part2 :: [String] -> Int
-part2 input = length $ takeWhile (\rows -> null (rows !! 0)) $ iterate (dropGrain2 (500, 0) floor) rows
-    where blocked = getStructures input
-          rows = convertToRows floor blocked
-          floor = (maximum $ map snd blocked) + 2
+part2 input = countWhile topRowIsEmpty structures
+    where structures = addFloor $ convertToRows $ getStructures input
+          topRowIsEmpty rows = null (rows !! 0)
 
-dropGrain2 :: Position -> Int -> [Row] -> [Row]
-dropGrain2 start floor blocked = addGrain2 (fall2 blocked floor start) blocked
+countWhile :: ([Row] -> Bool) -> [Row] -> Int
+countWhile condition structures = length $ takeWhile condition $ iterate dropGrain structures
 
-addGrain2 :: Position -> [Row] -> [Row]
-addGrain2 (x, y) rows = (take y rows) ++ [(x : (rows !! y))] ++ (drop (y+1) rows)
 
-fall2 :: [Row] -> Int -> Position -> Position
-fall2 blocked floor current@(x, y)
-    | y + 1 == floor = current
+dropGrain :: [Row] -> [Row]
+dropGrain blocked = addGrain (fall blocked start) blocked
+    where start = (500, 0)
+
+addGrain :: Position -> [Row] -> [Row]
+addGrain (x, y) rows = (take y rows) ++ [(x : (rows !! y))] ++ (drop (y+1) rows)
+
+fall :: [Row] -> Position -> Position
+fall blocked current@(x, y)
     | next == current = current
-    | otherwise = fall2 blocked floor next
+    | otherwise = fall blocked next
     where next = fallStep blocked current
-
 
 fallStep :: [Row] -> Position -> Position
 fallStep blocked current@(x, y)
@@ -71,9 +52,14 @@ fallStep blocked current@(x, y)
     where isFree x = all (/= x) (blocked !! (y + 1))
 
 
-convertToRows :: Int -> [Position] -> [Row]
-convertToRows floor positions = reverse $ foldl (\acc y -> (getRow y) : acc) [] [0..(floor-1)]
+convertToRows :: [Position] -> [Row]
+convertToRows positions = reverse $ foldl (\acc y -> (getRow y) : acc) [] [0..lowest]
     where getRow row = map fst $ filter (\(_, y) -> y == row) positions
+          lowest = maximum $ map snd positions
+
+addFloor :: [Row] -> [Row]
+addFloor structures = structures ++ [[], [(500 - height)..(500 + height)]]
+    where height = length structures + 2
 
 getStructures :: [String] -> [Position]
 getStructures input = concat $ map (buildStructure . points) input
